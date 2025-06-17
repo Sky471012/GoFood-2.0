357e64ba4e0aae4c113beef8439882fc53cbec6d
Sky471012 <sharma.aakash1012@gmail.com>
2025-06-17 16:54:40 +0530

    error resolved again

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,9 @@
+import heroImg from '../assets/hero_img.png';
+
 export default function Hero() {
 
     return (<div className="hero-container">
-        <img src="../src/assets/hero_img.png" alt="hero" className="hero_img" />
+        <img src={heroImg} alt="hero" className="hero_img" />
         <div className="hero_content">
             <h1 className="hero_title fst-italic"><b>GoFood</b></h1>
             <h4 className="hero_description">Craving Something Delicious? <br/> Get It Delivered Fast with <b><i>GoFood</i></b> </h4>
